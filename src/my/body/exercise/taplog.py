a239bf1585b76747f8961fc59012c9a523e3a318
Dima Gerasimov <karlicoss@gmail.com>
2020-10-13 19:39:54 +0100

    initial import of workout entries from taplog

---
+++ src/my/body/exercise/taplog.py
@@ -0,0 +1,43 @@
+from typing import Dict
+
+from ... import taplog as T
+from ...core import Res
+
+from my.config import exercise as user_config
+
+
+def overrides() -> Dict[str, str]:
+    '''
+    Manual overrides for some entries with typos etc, to simplify further automated parsing
+    '''
+    # to dump the initial table:
+    # sqlite3 taplog.db 'SELECT printf("| %6d | %s |", _id, lower(note)) FROM log WHERE cat1="ex" ORDER BY lower(note)'
+    # todo use orgmode provider directly??
+    from porg import Org
+    # TODO should use all org notes and just query from them?. it's quite slow...
+    wlog = Org.from_file(user_config.workout_log)
+    table = wlog.xpath('//org[heading="Taplog overrides"]//table')
+    res = {}
+    for row in table.lines:
+        id   = row['id']
+        note = row['note']
+        res[id] = note
+    return res
+
+
+def entries() -> Res[T.Entry]:
+    ov = overrides()
+    patched = 0
+    for e in T.by_button('ex'):
+        o = ov.get(e.id, None)
+        if o is not None:
+            e.row['note'] = o
+            patched += 1
+        yield e
+    if patched == 0:
+        yield RuntimeError('no mixins were matched')
+
+
+from ...core import stat, Stats
+def stats() -> Stats:
+    return stat(entries)
